f4db3883500e7ca14efccf2cd937c404dc62fc83
DamianPodrazaCode <damian.podraza@gmail.com>
2023-12-24 15:43:43 +0100

    Update fileSystem-folders.py

--- fileSystem-folders.py
+++ fileSystem-folders.py
@@ -1,1 +1,13 @@
 import os
+path = os.getcwd() #katalog roboczy
+print(path)
+
+os.chdir('d:/') #zmień katalog
+print(os.getcwd())
+
+os.chdir(path)
+print(os.getcwd())
+
+print(os.listdir()) # wylistowanie katalogu, pliki i katalogi
+for f in os.listdir():
+    print(f)
