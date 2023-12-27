1a91069538f3199996e0ab190afd937a0b349d63
DamianPodrazaCode <damian.podraza@gmail.com>
2023-12-27 18:39:34 +0100

    Update klasy.py and main.py

--- klasy/klasy.py
+++ klasy/klasy.py
@@ -11,14 +11,17 @@ class Klasa1:
         print(f'showAll {self.zm1}  {self.zm2}')
     
 
-class Klasa2(Klasa1):     #dziedzieczenie Klasa2 dziedziczy po Klasa1
+class Klasa2(Klasa1):  #dziedzieczenie Klasa2 dziedziczy po Klasa1
     zm3 = ''
     am4 = ''
 
     def __init__(self, zm1='zm1', zm2='zm2', zm3='zm3', zm4='zm4'):
+        # super().__init__(zm1, zm2)
+        self.zm1 = zm1
+        self.zm2 = zm2
         self.zm3 = zm3
         self.zm4 = zm4
         print(f'konstruktor {self.zm1}  {self.zm2} {self.zm3}  {self.zm4}')
 
     def showAll(self): #metoda, self jest wymagane ale z zewnątrz nie urzywane
-        print(f'showAll {self.zm3}  {self.zm4}')
+        print(f'showAll {self.zm1} {self.zm2} {self.zm3} {self.zm4}')
--- klasy/main.py
+++ klasy/main.py
@@ -3,14 +3,15 @@ from klasy import Klasa1
 from klasy import Klasa2
 
 def test():
-    b = Klasa1('kl1', 100)
-    b.showAll()
-    c = Klasa2('kl2', 12, 34, 56)
-    c.showAll()
+    obj1 = Klasa1('kl1', 100)
+    print(obj1) # adress objektu
+    obj1.showAll()
+    
+    obj2 = Klasa2('kl2', 12, 34, 56)
+    print(obj2) # adress objektu
+    obj2.showAll()
 
 if __name__ == "__main__":
-    print(Klasa1('test'))  #adres objektu
-    print(Klasa2('test'))  #adres objektu
     test()
     
 
