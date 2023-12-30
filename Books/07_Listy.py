7ae9a2836f4a151ece47f8d89f31993137c691d1
DamianPodrazaCode <damian.podraza@gmail.com>
2023-12-30 14:11:49 +0100

    Update 07_Listy.py

--- Books/07_Listy.py
+++ Books/07_Listy.py
@@ -1,5 +1,28 @@
+print('---------------------------------------------Lista')
+'''
+* jest sekwencja elementów
+* elementy mogą być dowolnego typu
+* mogą być mieszane na jednej liście
+* liste tworzy się za pomocą nawiasów kwadratowych []
+'''
+lista = [] # stworzenie listy pustej
+lista.append('asd') # dodanie elementu do listy
+lista.append(123) 
+lista.append(123.12) 
+lista.append(10 + 15j)
+lista.append([12, 3.4, 'ghj']) # dodanie listy jako elementu
+print(lista)
 
-# print('---------------------------------------------')
+lista = [12, 3.45, 'ytr', [1, 2, 3]] # stworzenie listy bezpośrednio
+print(lista)
+
+lista[0] = 'text' # bezpośredni dostęp do listy
+lista[1] = 12 + 6j
+print(lista)
+
+# sprawdzanie po wartości
+
+
 # print('---------------------------------------------')
 # print('---------------------------------------------')
 # print('---------------------------------------------')
