df70664e9a500929ea9580c431c3ca11e805fef9
DamianPodrazaCode <damian.podraza@gmail.com>
2024-01-04 14:16:11 +0100

    Update data.txt, mlpackTest.py, and test_AI.py

--- AI/test_AI.py
+++ AI/test_AI.py
@@ -1,9 +1,10 @@
 # Import necessary libraries
 import numpy as np
+from art import text2art
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from art import text2art
+
 
 # Function to print the introduction of the program
 def print_intro():
@@ -27,6 +28,7 @@ def print_intro():
 def load_data():
     # Load data from file, ignoring white spaces and accepting unlimited length numbers
     data = np.genfromtxt('data.txt', delimiter=',', dtype=int)
+    # print(data)
     # Replace all -1 values with 0
     data[data == -1] = 0
     # Split data into training and validation sets
