63dba0d4e1c345f3dd80d6d99af07a2ac4f3f844
DamianPodrazaCode <damian.podraza@gmail.com>
2024-01-23 20:28:51 +0100

    Update 00_begin1.py and 01_begin2.py

--- AI_ML_Py/Python_/00_begin1.py
+++ AI_ML_Py/Python_/00_begin1.py
@@ -290,6 +290,68 @@ print(mapa) # jeżeli niema pola do update to doda nowe {'a': 1234, 'b': 'hello'
 tLista = (1, 2, 3, 4, 5)
 print(tLista)
 print(tLista[4])
+# wszystko co w liscie oprócz zapisu
 
+# ------------------ sets -> nieuporządkowana kolekcja objektów, do szybkich operacji na zbiorach
+mySet = {1,2,3,4,5}
+print(mySet) # {1, 2, 3, 4, 5}
+mySet = {111,232,3.453,4.2,523}
+print(mySet) # {3.453, 4.2, 232, 523, 111}
+mySet = {111,232,3.453,4.2,523,111} # nie można dodać takiego samego elementu
+print(mySet) # {3.453, 4.2, 232, 523, 111}
+mySet.add(3.453)
+print(mySet) # {3.453, 4.2, 232, 523, 111}
 
+# usunięcie z lisy powtarzających się elementów
+myList = [1,2,3,4,5,2,3,4,5]
+print(myList) # [1, 2, 3, 4, 5, 2, 3, 4, 5]
+mySet = set(myList)
+myList = list(mySet)
+print(myList) # [1, 2, 3, 4, 5]
 
+# set - brak dostępu przez index
+print(2 in mySet) # True
+
+# -----------------
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8,9,10}
+print(set1.difference(set2)) # {1, 2, 3} wyświetliło różnice (różnica bez zmian w set1, set2)
+print(set1.discard(5)) # usunięcie 5
+print(set1) # {1, 2, 3, 4}
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8,9,10}
+set1.difference_update(set2)
+print(set1) # {1, 2, 3} usuneło wszystko co było takiesamo w set2 (różnica ze zmianami w set1)
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8,9,10}
+print(set1.intersection(set2)) # {4, 5} część wspólna bez zmian w set1
+print(set1) # {1, 2, 3, 4, 5}
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8,9,10}
+set1.intersection_update(set2) 
+print(set1) # {4, 5} część wspólna ze zmianą w set1
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8,9,10}
+print(set1.isdisjoint(set2)) # czy zbiory są rozłączne False
+
+set1 = {1,2,3}
+set2 = {4,5,6,7,8,9,10}
+print(set1.isdisjoint(set2)) # czy zbiory są rozłączne True
+
+set1 = {4,5,6}
+set2 = {4,5,6,7,8,9,10}
+print(set1.issubset(set2)) # czy set1 jest podzbiorem set2
+
+set1 = {4,5,6,7}
+set2 = {4,5}
+print(set1.issuperset(set2)) # czy set2 jest podzbiorem set1
+
+set1 = {4,5,6,7}
+set2 = {4,5,10,11,12}
+print(set1.union(set2)) # łączenie {4, 5, 6, 7, 10, 11, 12}
+print(set1 | set2) # jw {4, 5, 6, 7, 10, 11, 12}
+print(set1 & set2) # {4, 5}
