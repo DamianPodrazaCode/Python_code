90c05ab53ba0ef64cef3cf15cab09398a71db089
DamianPodrazaCode <damian.podraza@gmail.com>
2024-01-31 22:00:23 +0100

    Update 03_numpy.py

--- AI_ML_Py/Python_/03_numpy.py
+++ AI_ML_Py/Python_/03_numpy.py
@@ -139,13 +139,341 @@ print(random_array_5)
 #  [7.78765841e-04 9.92211559e-01 6.17481510e-01]
 #  [6.11653160e-01 7.06630522e-03 2.30624250e-02]]
 
+# -----------------------------------------------------------------------------
+
+# viewing arraus, matrices
+
+print(np.unique(random_array_4)) # wyświetlenie jakie są elementy w tablicy
+# [2 3 4 5 6 7 9]
+
+print('\n', a1, '\n\n', a2, '\n\n', a3)
+#  [1 2 3]
+
+#  [[1.  2.  3.2]
+#  [4.  5.  6.4]]
+
+#  [[[ 1  2  3]
+#   [ 4  5  6]
+#   [ 7  8  9]]
+
+#  [[10 11 12]
+#   [13 14 15]
+#   [16 17 18]]]
+
+print(a1[0])
+# 1
+
+print(a2[0])
+# [1.  2.  3.2]
+
+print(a3[0])
+# [[1 2 3]
+#  [4 5 6]
+#  [7 8 9]]
+
+print(a3[:2, :2, :2])
+# [[[ 1  2]
+#   [ 4  5]]
+
+#  [[10 11]
+#   [13 14]]]
+
+a4 = np.random.randint(10, size=(2, 3, 4, 5))
+print(a4)
+# [[[[9 2 6 3 8]
+#    [2 4 2 6 4]
+#    [8 6 1 3 8]
+#    [1 9 8 9 4]]
+
+#   [[1 3 6 7 2]
+#    [0 3 1 7 3]
+#    [1 5 5 9 3]
+#    [5 1 9 1 9]]
+
+#   [[3 7 6 8 7]
+#    [4 1 4 7 9]
+#    [8 8 0 8 6]
+#    [8 7 0 7 7]]]
+
+
+#  [[[2 0 7 2 2]
+#    [0 4 9 6 9]
+#    [8 6 8 7 1]
+#    [0 6 6 7 4]]
+
+#   [[2 7 5 2 0]
+#    [2 4 2 0 4]
+#    [9 6 6 8 9]
+#    [9 2 6 0 3]]
+
+#   [[3 4 6 6 3]
+#    [6 2 5 1 9]
+#    [8 4 5 3 9]
+#    [6 8 6 0 0]]]]
+
+print(a4[:, :, :, :4])
+# [[[[9 2 6 3]
+#    [2 4 2 6]
+#    [8 6 1 3]
+#    [1 9 8 9]]
+
+#   [[1 3 6 7]
+#    [0 3 1 7]
+#    [1 5 5 9]
+#    [5 1 9 1]]
+
+#   [[3 7 6 8]
+#    [4 1 4 7]
+#    [8 8 0 8]
+#    [8 7 0 7]]]
+
+
+#  [[[2 0 7 2]
+#    [0 4 9 6]
+#    [8 6 8 7]
+#    [0 6 6 7]]
+
+#   [[2 7 5 2]
+#    [2 4 2 0]
+#    [9 6 6 8]
+#    [9 2 6 0]]
+
+#   [[3 4 6 6]
+#    [6 2 5 1]
+#    [8 4 5 3]
+#    [6 8 6 0]]]]
 
 # -----------------------------------------------------------------------------
+# Manipulate arrays
+
+print(a1)
+# [1 2 3]
+
+print(a1 + 1)
+# [2 3 4]
+
+ones = np.ones(3)
+print(ones)
+# [1. 1. 1.]
+
+print(a1 + ones)
+# [2. 3. 4.]
+
+print(a1 - ones)
+# [0. 1. 2.]
+
+print(a2)
+# [[1.  2.  3.2]
+#  [4.  5.  6.4]]
+
+print(a1 * a2)
+# [[ 1.   4.   9.6]
+#  [ 4.  10.  19.2]]
+
+print(a1 / ones)
+# [1. 2. 3.]
+
+print(a2 / a1)
+# [[1.         1.         1.06666667]
+#  [4.         2.5        2.13333333]]
+
+print(a2 // a1)  # dzielenie bez reszt (obcinanie do dołu)
+# [[1. 1. 1.]
+#  [4. 2. 2.]]
+
+print(a2 ** 2) # potęga 2
+# [[ 1.    4.   10.24]
+#  [16.   25.   40.96]]
+
+print(np.square(a2)) # też potęga 2
+# [[ 1.    4.   10.24]
+#  [16.   25.   40.96]]
+
+print(np.add(a1, a2))
+# [[2.  4.  6.2]
+#  [5.  7.  9.4]]
+
+print(a2 % a1) # reszta z dzielenia
+# [[0.  0.  0.2]
+#  [0.  1.  0.4]]
+
+print(np.exp(a1))
+# [ 2.71828183  7.3890561  20.08553692]
+
+print(np.log(a1))
+# [0.         0.69314718 1.09861229]
+
 # -----------------------------------------------------------------------------
+# Aggregation
+
+listy_list = [1, 2, 3]
+print(type(listy_list))
+# <class 'list'>
+
+print(sum(listy_list))
+# 6
+
+print(a1)
+# [1 2 3]
+print(sum(a1))
+# 6
+print(np.sum(a1))
+# 6
+# taki sam wynik, ale powinno się używać m,etod numpy na danych numpy, a metod pythona na danych pythona
+
+# wielkie tablice
+massive_array = np.random.random(1000000)
+print(massive_array.size)
+# 100000
+
+print(massive_array[:10])
+# [0.22793516 0.42710779 0.81801477 0.86073058 0.00695213 0.5107473
+#  0.417411   0.22210781 0.11986537 0.33761517]
+
+import time
+start = time.time()
+sum(massive_array)
+end = time.time()
+print(end - start)
+# 0.09500288963317871 [s]
+# 95 [ms]
+
+start = time.time()
+np.sum(massive_array)
+end = time.time()
+print(end - start)
+# 0.0020046234130859375 [s]
+# 2 [ms]
+
+print(a2)
+# [[1.  2.  3.2]
+#  [4.  5.  6.4]]
+
+print(np.mean(a2)) # średnia tablicy
+# 3.6
+
+# Standard deviation to miara rozproszenia danych wokół ich średniej arytmetycznej. Jest to jedna z najczęściej używanych miar w statystyce.
+# Standard deviation jest obliczane jako pierwiastek kwadratowy wariancji. 
+# Wariancja mierzy, jak bardzo dane różnią się od średniej, a standard deviation dostarcza miary tego rozrzutu w jednostkach oryginalnych danych.
+print(np.std(a2)) # standardowe odchylenie
+# 1.8
+
+# Wariancja (Variance) - W statystyce wariancja to miara, która określa, 
+# jak bardzo wartości zbioru danych różnią się od średniej arytmetycznej tych wartości. 
+# Im większa wariancja, tym większe są różnice między poszczególnymi wartościami a średnią.
+print(np.var(a2)) # variance 
+# 3.24
+
+print(np.sqrt(np.var(a2))) # standardowe odchylenie
+# 1.8
+
 # -----------------------------------------------------------------------------
+# STD and VAR
+
+high_var_array = np.array([1, 100, 200, 300, 4000, 5000])
+low_var_array = np.array([2, 4, 6, 8, 10])
+
+print(np.var(high_var_array))
+# 4296133.472222221
+print(np.var(low_var_array))
+# 8.0
+
+print(np.std(high_var_array))
+# 2072.711623024829
+print(np.std(low_var_array))
+# 2.8284271247461903
+
+print(np.mean(high_var_array))
+# 1600.1666666666667
+print(np.mean(low_var_array))
+# 6.0
+
+import matplotlib.pyplot as plt 
+# plt.figure(1)
+# plt.hist(high_var_array)
+# plt.plot(high_var_array)
+# plt.figure(2)
+# plt.hist(low_var_array)
+# plt.plot(low_var_array)
+# plt.show()
+
 # -----------------------------------------------------------------------------
+# Reshape and Transposing
+
+print(a2)
+# [[1.  2.  3.2]
+#  [4.  5.  6.4]]
+
+print(a3)
+# [[[ 1  2  3]
+#   [ 4  5  6]
+#   [ 7  8  9]]
+
+#  [[10 11 12]
+#   [13 14 15]
+#   [16 17 18]]]
+
+print(a2.shape, a3.shape)
+# (2, 3) (2, 3, 3)
+
+# można mnożyć tablice:
+# - jak kształty dwóch tablic są takiesame
+# - jeżeli nie są równe to jeden wymiar musi mieć 1
+
+
+a2_reshape = a2.reshape(2, 3, 1)
+print(a2)
+# [[[1. ]
+#   [2. ]
+#   [3.2]]
+
+#  [[4. ]
+#   [5. ]
+#   [6.4]]]
+print(a3)
+# [[[ 1  2  3]
+#   [ 4  5  6]
+#   [ 7  8  9]]
+
+#  [[10 11 12]
+#   [13 14 15]
+#   [16 17 18]]]
+
+print(a2_reshape * a3)
+# [[[  1.    2.    3. ]
+#   [  8.   10.   12. ]
+#   [ 22.4  25.6  28.8]]
+
+#  [[ 40.   44.   48. ]
+#   [ 65.   70.   75. ]
+#   [102.4 108.8 115.2]]]
+
 # -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
+
+# Transpose (transpozycja) to operacja matematyczna polegająca na zamianie miejscami kolumn i wierszy w macierzy.
+print(a3)
+# [[[ 1  2  3]
+#   [ 4  5  6]
+#   [ 7  8  9]]
+
+#  [[10 11 12]
+#   [13 14 15]
+#   [16 17 18]]]
+
+# print(a3.transpose())
+print(a3.T) # to co wyżej
+# [[[ 1 10]
+#   [ 4 13]
+#   [ 7 16]]
+
+#  [[ 2 11]
+#   [ 5 14]
+#   [ 8 17]]
+
+#  [[ 3 12]
+#   [ 6 15]
+#   [ 9 18]]]
+
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
