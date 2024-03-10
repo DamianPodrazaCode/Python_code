c2a748e1f01a3c6d3331bbb3a30b7293de6fd730
DamianPodrazaCode <damian.podraza@gmail.com>
2024-03-10 22:20:19 +0100

    Update 02_FlowControl.ipynb, 02_WykonanieWarunkowe.py, and 11_Wyjątki.py

--- TEMP/Books/02_WykonanieWarunkowe.py
+++
@@ -1,9 +0,0 @@
-# try - except (jeżeli w 'try' wystąpi wyjątek to wykona się exept)
-inp = input('Podaj temperaturę w skali Fahrenheita: ')
-try:
-    fahr = float(inp) # cast z worowadzonej liczby na float, tu może wystąpić wyjątek
-    cel = (fahr - 32.0) * 5.0 / 9.0
-    print(cel)
-except:
-    print('Musisz wprowadzić liczbę')
-
