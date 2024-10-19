e7d73957cf8e2b91a63680983ed32a68e93a088f
DamianPodrazaCode <damian.podraza@gmail.com>
2024-10-19 19:00:53 +0200

    Update dialog.py, main.py, ui_Dialog.py, and 22 more files

--- VisualStudioCode/PySide6-QtDesigner/00_Dialog/dialog.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget
-from ui_Dialog import Ui_Dialog  # import klasy wygenerowanej z QtDesigner (ui_Dialog.py)
-
-class OknoDialog(QWidget, Ui_Dialog): # wszystkie widgety dziedziczone z Ui_Dialog
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self) # z dziedziczenia, inicjalizacja okna
--- VisualStudioCode/PySide6-QtDesigner/00_Widget/widget.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget
-from ui_Widget import Ui_Form  # import klasy wygenerowanej z QtDesigner (ui_Widget.py)
-
-class Okno(QWidget, Ui_Form): # wszystkie widgety dziedziczone z Ui_Form
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self) # z dziedziczenia, inicjalizacja okna
