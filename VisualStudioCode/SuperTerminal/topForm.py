041c344ab0e2cb722bd22e38f789f4fcd855d376
DamianPodrazaCode <damian.podraza@gmail.com>
2024-11-03 21:55:54 +0100

    Update topForm.py

--- VisualStudioCode/SuperTerminal/topForm.py
+++ VisualStudioCode/SuperTerminal/topForm.py
@@ -1,17 +1,17 @@
 from PySide6.QtWidgets import QWidget, QTreeWidget, QTreeWidgetItem
+from PySide6.QtSerialPort import QSerialPortInfo, QSerialPort
+from PySide6.QtCore import QIODevice, Signal, Slot
 from ui_topForm import Ui_TopForm 
 from serialForm import serialPortWindow
-import serial.tools.list_ports
 
 class mainWindow(QWidget, Ui_TopForm) :
     def __init__(self) :
         super().__init__()
+        self.setupUi(self) # setup onka z designera
 
-        self.windowsSerial = []
+        self.windowsSerial = [] # lista obiektów z nowym oknem
 
-        self.setupUi(self)
-
-        self.twSerial.setHeaderLabels(["Device", "Description", "Manufacturer", "Hwid"])
+        self.twSerial.setHeaderLabels(["Port Name", "Description", "Manufacturer", "Serial Number", "System Location", "PID", "VID"])
         self.pbScanSerial_clicked()
 
         self.pbScanSerial.clicked.connect(self.pbScanSerial_clicked)
@@ -20,9 +20,16 @@ class mainWindow(QWidget, Ui_TopForm) :
        
     def pbScanSerial_clicked(self) :
         self.twSerial.clear()
-        ports = serial.tools.list_ports.comports()
+        ports = QSerialPortInfo.availablePorts()
         for port in ports : 
-            item = QTreeWidgetItem([port.device, port.description, port.manufacturer, port.hwid])        
+            d1 = port.portName()
+            d2 = port.description()
+            d3 = port.manufacturer()
+            d4 = port.serialNumber()
+            d5 = port.systemLocation()
+            d6 = port.productIdentifier()
+            d7 = port.vendorIdentifier()
+            item =  QTreeWidgetItem([d1, d2, d3, d4, d5, d6, d7])
             self.twSerial.addTopLevelItem(item) 
         for column in range(self.twSerial.columnCount()) :  
             self.twSerial.resizeColumnToContents(column)    
